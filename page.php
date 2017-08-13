68732f93736a58fec071c8d035070cea308d161e
deuce452 <keonte.davis@drivenkonnections.com>
2017-08-13 16:15:38 -0400

    added new page templates - page-notitle - page-services - page

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+    Template Name: Basic Page Template
+*/
+
+get_header(); ?>
+
+	<?php
+		if(have_posts()):
+			while(have_posts()): the_post(); ?>
+
+				<p><?php the_content();?></p>
+
+			<?php endwhile;
+		endif; 
+	?>
+
+<?php get_footer(); ?>
