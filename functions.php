6a83ac5d148d347b8baf197f3dfce67ad8a5514f
deuce452 <keonte.davis@drivenkonnections.com>
2017-08-13 19:45:15 -0400

    updated functions.php to integrate new css and js files

--- functions.php
+++ functions.php
@@ -1,12 +1,30 @@
 <?php
+
+/*
+	==================
+	Include Scripts
+	==================
+*/
 	
 function driv_elite_script_enqueue() {
-	
+	//css
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '1.0.0', 'all');
 	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/driven-elite.css', array(), '1.0.0', 'all');
+	
+	//js
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true);
 	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/driven-elite.js', array(), '1.0.0', true);
 	
 }
 add_action( 'wp_enqueue_scripts', 'driv_elite_script_enqueue');
+
+
+/*
+	==================
+	Active Menus
+	==================
+*/
+
 function driv_elite_theme_setup() {
 	
 	add_theme_support('menus');
@@ -17,6 +35,12 @@ function driv_elite_theme_setup() {
 }
 add_action('init', 'driv_elite_theme_setup');
 
+
+/*
+	==================
+	Theme Support Functions
+	==================
+*/
 add_theme_support('title-tag');
 add_theme_support('custom-background');
 add_theme_support('custom-header');
