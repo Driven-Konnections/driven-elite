aae79d180b02486670b21d15653d66f080aeb700
deuce452 <keonte.davis@drivenkonnections.com>
2017-08-13 20:05:57 -0400

    updated rerst of post format pages

--- post-content-video.php
+++ post-content-video.php
@@ -1,8 +1,14 @@
-    <h3>VIDEO POST: <?php the_title();?></h3>
-    
-    <small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a');?>, in 
-    <?php the_category();?></small>
+<article id="post-<?php the_ID();?>" <?php post_class();?>>
 
-    <p><?php the_content();?></p>
+    <header class="entry-header">
+        <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+        <small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a');?>, in <?php the_category();?></small>
+    </header>
 
-    <hr>
+    <div class="row">
+        <div class="col-xs-12">
+			<?php the_content();?>
+        </div>
+    </div>
+
+</article>
